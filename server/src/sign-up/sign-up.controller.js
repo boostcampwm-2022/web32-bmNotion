d446da4eca6ee9a82cce58bc60d7248518983f92
ingyobot <woo981216@gmail.com>
2022-11-15 17:47:40 +0900

    feat:추가 회원가입 시 아이디 닉네임 중복 검사 추상화
    mongoDB CRUD가 미구현 되어 있어 아이디 닉네임 중복 검사 추상화
    
    fixes:#11

--- server/src/sign-up/sign-up.controller.js
+++ server/src/sign-up/sign-up.controller.js
@@ -1,14 +1,23 @@
 const multer = require('multer');
-const uploadImg = require('./sign-up.service');
+const { uploadImg, searchUser, saveUser } = require('./sign-up.service');
 
 const signUpController = {
   readProfileImg: multer({ storage: multer.memoryStorage() }).single('img'),
   signUp: (req, res) => {
-    const { id, password } = req.body;
-    const { file } = req;
-    const objectName = `${id}.profile`;
-    uploadImg(objectName, file);
-    res.send(200);
+    const { id, password, nickname } = req.body;
+    const user = searchUser(id, nickname);
+    if (!user) {
+      const { file } = req;
+      const objectName = `${id}.profile`;
+      saveUser(id, password, nickname, objectName);
+      uploadImg(objectName, file);
+      res.send(200);
+    } else if (user.nickname === nickname) {
+      res.send('exist nickname');
+    } else if (user.id === id) {
+      res.send('exist id');
+    }
+    res.send(500);
   },
 };
 
--- server/src/sign-up/sign-up.service.js
+++ server/src/sign-up/sign-up.service.js
@@ -2,6 +2,7 @@ const AWS = require('aws-sdk');
 const stream = require('node:stream');
 
 const region = 'kr-standard';
+const bucketName = 'bmnotion';
 const accessKey = `${process.env.ACCESS_KEY}`;
 const secretKey = `${process.env.SECRET_KEY}`;
 const S3 = new AWS.S3({
@@ -14,7 +15,6 @@ const S3 = new AWS.S3({
 });
 
 const uploadImg = async (objectName, file) => {
-  const bucketName = 'bmnotion';
   await S3.upload({
     Bucket: bucketName,
     Key: objectName,
@@ -22,4 +22,37 @@ const uploadImg = async (objectName, file) => {
   }).promise();
 };
 
-module.exports = uploadImg;
+const searchUser = (id, nickname) => {
+  // todo
+  // search user by id or nickname
+  const user = [id, nickname];
+  return user;
+};
+
+const createObjectUrl = (objectName) => {
+  const params = { Bucket: bucketName, Key: objectName };
+  const url = S3.getSignedUrl('getObject', params);
+  return url;
+};
+
+const encryptPassword = (password) => {
+  // todo
+  // password encrypt logic
+  const encrypted = password;
+  return encrypted;
+};
+
+const saveUser = (id, password, nickname, objectName) => {
+  // todo
+  // insert User info
+  const encryptedPw = encryptPassword(password);
+  const user = [id, nickname, encryptedPw, objectName];
+  return user;
+};
+
+module.exports = {
+  uploadImg,
+  searchUser,
+  saveUser,
+  createObjectUrl,
+};
