56d67c901f0a4cadf673b91b814ee501fe275045
Jeon <jeoninseong8140@gmail.com>
2022-11-16 21:01:40 +0900

    feat: dotenv 설치 및 사용
    dotenv 설치 및 server 적용

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,8 @@ const mainRouter = require('./main/main.router.js');
 const port = process.env.PORT || '8080';
 const app = express();
 
+require('dotenv').config();
+
 app.use(express.static(path.resolve(__dirname, '..', 'client', 'dist')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
