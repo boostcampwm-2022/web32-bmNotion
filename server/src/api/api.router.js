78de2a5b80fba8f330667e0abcd70b96d2c44d57
ingyobot <woo981216@gmail.com>
2022-11-17 04:32:12 +0900

    [#11&#17]feat: BE login&sign-up API 라우터
    로그인 회원가입 API 라우터 추가
    
    fixes:#11, fixes:#17

--- server/src/api/api.router.js
+++ server/src/api/api.router.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const signUpRouter = require('../sign-up/sign-up.router');
+const loginRouter = require('../login/login.router');
 
 const router = Router();
 
-router.use('/sign-up', signUpRouter);
+router.use('/auth/login', loginRouter);
+router.use('/auth/sign-up', signUpRouter);
 
 module.exports = router;
