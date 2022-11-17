270cf89d17047d62142a7db5eff6d8caae50e640
ingyobot <woo981216@gmail.com>
2022-11-18 01:07:28 +0900

    feat: 회원가입 API response
    회원가입 api의 response를 명세에 맞춰 수정

--- server/src/sign-up/sign-up.controller.js
+++ server/src/sign-up/sign-up.controller.js
@@ -1,23 +1,13 @@
 const multer = require('multer');
-const { uploadImg, searchUser, saveUser } = require('./sign-up.service');
+const { signUpPipeline } = require('./sign-up.service');
 
 const signUpController = {
-  readProfileImg: multer({ storage: multer.memoryStorage() }).single('img'),
-  signUp: (req, res) => {
-    const { id, password, nickname } = req.body;
-    const user = searchUser(id, nickname);
-    if (!user) {
-      const { file } = req;
-      const objectName = `${id}.profile`;
-      saveUser(id, password, nickname, objectName);
-      uploadImg(objectName, file);
-      res.send(200);
-    } else if (user.nickname === nickname) {
-      res.send('exist nickname');
-    } else if (user.id === id) {
-      res.send('exist id');
-    }
-    res.send(500);
+  readProfileImg: multer({ storage: multer.memoryStorage() }).single('image'),
+  signUp: async (req, res) => {
+    const { file, body } = req;
+    const { id, passWord, nickName } = body;
+    const resJson = await signUpPipeline(id, passWord, nickName, file);
+    res.json(resJson);
   },
 };
 
--- server/src/sign-up/sign-up.service.js
+++ server/src/sign-up/sign-up.service.js
@@ -1,5 +1,6 @@
 const AWS = require('aws-sdk');
 const stream = require('node:stream');
+const { createDocument } = require('../db/db.crud');
 
 const region = 'kr-standard';
 const bucketName = 'bmnotion';
@@ -15,6 +16,7 @@ const S3 = new AWS.S3({
 });
 
 const uploadImg = async (objectName, file) => {
+  if (file === undefined) return;
   await S3.upload({
     Bucket: bucketName,
     Key: objectName,
@@ -26,7 +28,7 @@ const searchUser = (id, nickname) => {
   // todo
   // search user by id or nickname
   const user = [id, nickname];
-  return user;
+  return undefined;
 };
 
 const createObjectUrl = (objectName) => {
@@ -42,17 +44,57 @@ const encryptPassword = (password) => {
   return encrypted;
 };
 
-const saveUser = (id, password, nickname, objectName) => {
+const saveUser = async (id, password, nickname, objectName) => {
   // todo
   // insert User info
   const encryptedPw = encryptPassword(password);
-  const user = [id, nickname, encryptedPw, objectName];
+  const user = {
+    id,
+    nickname,
+    encryptedPw,
+    objectName,
+  };
+  await createDocument('user', user);
   return user;
 };
 
+const createResponse = (message) => {
+  const response = { code: '500', message: '' };
+  switch (message) {
+    case 'success':
+      response.code = 202;
+      response.message = 'Success';
+      break;
+    case 'exist nickname':
+      response.code = 404;
+      response.message = { nickname: '이미 사용중인 닉네임입니다.' };
+      break;
+    case 'exist id':
+      response.code = 404;
+      response.message = { nickname: '이미 사용중인 아이디입니다.' };
+      break;
+    default:
+      break;
+  }
+  return response;
+};
+
+const signUpPipeline = async (id, password, nickname, file) => {
+  const user = searchUser(id, nickname);
+  let message = '';
+  if (!user) {
+    const objectName = `${id}.profile`;
+    await uploadImg(objectName, file);
+    await saveUser(id, password, nickname, objectName);
+    message = 'success';
+  } else if (user.nickname === nickname) {
+    message = 'exist nickname';
+  } else if (user.id === id) {
+    message = 'exist id';
+  }
+  return createResponse(message);
+};
+
 module.exports = {
-  uploadImg,
-  searchUser,
-  saveUser,
-  createObjectUrl,
+  signUpPipeline,
 };
