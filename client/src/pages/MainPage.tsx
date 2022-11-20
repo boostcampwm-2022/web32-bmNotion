dfaa783927ba73cfef610a570ff521c4bc49f82b
hhnn0 <hyun0520ho@gmail.com>
2022-11-21 04:10:05 +0900

    [#24]feat: sidebar 슬라이드 메뉴 추가

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -6,6 +6,11 @@ interface SideBarButtonProps {
   sideBarHoverButton: string;
 }
 
+interface SideBarProps {
+  isClicked: boolean;
+  sideBarHoverButton: string;
+}
+
 const hamburgerButton = "/assets/icons/hamburger.png";
 const doubleArrowButton = "/assets/icons/doubleArrow.png";
 const reverseDoubleArrowButton="/assets/icons/reverseDoubleArrow.png"
@@ -22,34 +27,47 @@ export default function MainPage():ReactElement {
   }
   return (
   <Wrapper>
-    <TopBar>
-      <TopBarLeft>
-        <SideBarButton isClicked={sideBarButtonClicked} sideBarButton={sideBarButton} sideBarHoverButton={"asd"} onClick={sideBarButtonClick}></SideBarButton>
-      </TopBarLeft>
-      <TopBarRight>오</TopBarRight>
-    </TopBar>
-    메인페이지입니다.
+    <SideBar isClicked={sideBarButtonClicked} sideBarHoverButton={reverseDoubleArrowButton}>
+      <SideBarHeaderContainer>     
+        <SideBarHeader>
+          <SideBarButton isClicked={!sideBarButtonClicked} sideBarButton={tranParentButton} sideBarHoverButton={reverseDoubleArrowButton} onClick={sideBarButtonClick}></SideBarButton>
+        </SideBarHeader>
+      </SideBarHeaderContainer>
+    </SideBar>
+    <MainContainer>
+      <TopBar>
+        <TopBarLeft>
+          <SideBarButton isClicked={sideBarButtonClicked} sideBarButton={hamburgerButton} sideBarHoverButton={doubleArrowButton} onClick={sideBarButtonClick}></SideBarButton>
+        </TopBarLeft>
+        <TopBarRight>오</TopBarRight>
+      </TopBar>
+      <MainContainerBody>메인페이지입니다.</MainContainerBody>
+    </MainContainer>
   </Wrapper>);
 }
 
 const Wrapper = styled.div`
   display:flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: row;
   width:100%;
   height: 100vh;
+`
+const MainContainer = styled.div`
+  display:flex;
+  flex-direction: column;
+  flex:1;
   position: relative;
 `
-
 const TopBar = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   position: absolute;
-  width: 100%;
+  width:100%;
   height: 45px;
   top:0;
 `
+
 const TopBarLeft = styled.div`
   display:flex;
 
@@ -61,16 +79,66 @@ const TopBarRight = styled.div`
     padding: 12px;
 `
 
-const SideBarButton = styled.button<SideBarButtonProps>`
-  background-image: url(${(props) => props.isClicked?tranParentButton:hamburgerButton});
-  background-repeat: no-repeat;
-  background-size:cover;
-  width:16px;
-  height:16px;
+const MainContainerBody = styled.div`
+  background-color: red;
+  width:100%;
+  flex:1;
+  margin-top:45px;
+`
 
+const SideBarButton = styled.button<SideBarButtonProps>`
+  display:${(props)=>props.isClicked? "none":""};
+  background-image: url(${(props) => props.sideBarButton});
+  background-repeat: no-repeat;
+  background-size:16px 16px;
+  background-position: center;
+  width:24px;
+  height:24px;
   transition: all 0.2s linear;
+  border-radius: 3px;
   
   &:hover {
-    background-image: url(${(props) => props.isClicked?reverseDoubleArrowButton:doubleArrowButton});
+    background-image: url(${(props) => props.sideBarHoverButton});
+    background-color:rgba(55, 53, 47, 0.08);
   }
+`
+
+const SideBar = styled.div<SideBarProps>`
+  margin-left:${(props)=>props.isClicked? "0px":"-240px"};
+  width:240px;
+  height:100%;
+  transition: 0.3s;
+  background-color: #FBFBFA;
+
+  &:hover {
+    ${SideBarButton} {
+      background-image:url(${(props)=>props.sideBarHoverButton});
+    }
+  }
+`
+
+const SideBarHeaderContainer = styled.div`
+  display:flex;
+  flex-direction: row;
+  align-items: center;
+  width:100%;
+  height: 45px;
+
+  &:hover {
+    background-color:#EBEBEA;
+  }
+`
+
+const SideBarHeader = styled.div`
+  display:flex;
+  align-items: center;
+  height:40px;
+  width:100%;
+  justify-content: flex-end;
+  padding:12px;
+`
+
+const SideBarBody = styled.div`
+  
+
 `
