ccbcd10c69aa1a9ae789e7b800da74ae9f53b9d1
jhj9109 <jhj91_09@naver.com>
2022-11-23 16:16:33 +0900

    Chore: 코드 기능 변화 없이 리팩토링

--- client/src/components/BlockContent.tsx
+++ client/src/components/BlockContent.tsx
@@ -53,24 +53,27 @@ export default function BlockContent({ children, blockId }: BlockContentProps): 
       /* TODO: 새로운 블록 생성하는 로직추가 */
     }
   };
-  const handleOnSpace = (e: any) => {
+  const handleOnSpace = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    /* 현재 카렛 위치 기준으로 text 분리 */
+    const elem = e.target as HTMLElement;
+    const content = elem.textContent || '';
     const offset = (window.getSelection() as Selection).focusOffset;
-    const preText = e.target.textContent.slice(0, offset);
-    const afterText = e.target.textContent.slice(offset);
+    const [preText, postText] = [content.slice(0, offset), content.slice(offset)];
 
-    if (/^#{1,3}$/.test(preText)) {
-      console.log(`H${preText.length}`, '로 타입을 변경하도록 합니다.');
-      e.target.textContent = afterText;
+    /* 마크다운 문법과 일치 => 해당 타입으로 변경 */
+    const toType = checkMarkDownGrammer(preText);
+    if (toType !== '') {
+      /* toType으로 타입변경 */
+      elem.textContent = postText;
       e.preventDefault();
-    } else {
-      console.log('아무일도 없었습니다.');
+      console.log(`toType => ${toType}`);/* TODO toType으로 타입변경하는 함수로 변경 필요 */
     }
-    console.log('스페이스 눌린 타이밍에서 컨텐츠의 값음', `|${(e.target as any).textContent}|`);
+    // console.log('스페이스 눌린 타이밍에서 컨텐츠의 값음', `|${(e.target as any).textContent}|`);
   };
-  const handleOnKeyDown = (e: any) => {
-    if (e.keyCode === 13) {
+  const handleOnKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.code === 'Enter') {
       handleOnEnter(e);
-    } else if (e.keyCode === 32) {
+    } else if (e.code === 'Space') {
       handleOnSpace(e);
     }
   };
@@ -80,13 +83,16 @@ export default function BlockContent({ children, blockId }: BlockContentProps): 
       // type => css
       contentEditable
       onKeyDown={handleOnKeyDown}
+      onInput={(e) => {
+        /* 디버깅용 */ console.log(`|${(e.target as Node).textContent}|`);
+      }}
     ></BlockContentBox>
   );
 }
 
 const BlockContentBox = styled.div.attrs({
   placeholder: 'hello',
-})<BlockContentBoxProps>`
+}) <BlockContentBoxProps>`
   &:empty::before {
     content: attr(placeholder);
     margin: 0 10px;
