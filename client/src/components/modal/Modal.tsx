9ddc3d7dd03002529e2ebc7533b01d8b8b1809b8
hhnn0 <hyun0520ho@gmail.com>
2022-11-23 18:11:30 +0900

    [#52] feat: 모달 컴포넌트 생성, topbar option 모달 생성

---
+++ client/src/components/modal/Modal.tsx
@@ -0,0 +1,34 @@
+import React, { useState, ReactElement, useEffect } from 'react';
+import styled from 'styled-components';
+
+interface ModalProps {
+  children?: ReactElement;
+  width: string;
+  height: string;
+
+}
+
+export default function Modal({children, width, height}:ModalProps): ReactElement {
+
+  return (
+    <ModalContainer>
+      <ModalBox width={width} height={height}>
+        {children}
+      </ModalBox>
+    </ModalContainer>
+  );
+}
+
+const ModalContainer = styled.div`
+  
+
+`
+const ModalBox = styled.div<ModalProps>`
+background-color: white;
+border-radius: 4px;
+  width: ${(props)=>props.width};
+  height: ${(props)=>props.height};
+  position:absolute;
+  box-shadow: rgb(15 15 15 / 5%) 0px 0px 0px 1px, rgb(15 15 15 / 10%) 0px 3px 6px, rgb(15 15 15 / 20%) 0px 9px 24px;
+  right:12px;
+`
