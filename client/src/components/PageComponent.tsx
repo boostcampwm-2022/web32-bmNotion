17f43203d5f092739b63c79b618ed7cb73f0a479
jhj9109 <jhj91_09@naver.com>
2022-11-24 14:34:17 +0900

    feat: 블록 추가 & 변경 로직

--- client/src/components/PageComponent.tsx
+++ client/src/components/PageComponent.tsx
@@ -1,20 +1,60 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import styled from 'styled-components';
 import BlockContent from '@/components/BlockContent';
 
+interface BlockInfo {
+    blockId: number;
+    content: string;
+    index: number;
+    type: string;
+}
+
+interface PageInfo {
+    nextId: number;
+    pageId: string;
+    blocks: BlockInfo[];
+}
+
+const sampleBlocks: BlockInfo[] = [
+    { blockId: 1, index: 1, content: "", type: "TEXT" },
+]
+
+const samplePageInfo: PageInfo = {
+    nextId: 2,
+    pageId: "abc",
+    blocks: sampleBlocks,
+}
+
 export default function PageComponent(): React.ReactElement {
-    const moveNextBlock = () => { };
+    const [pageInfo, setPageInfo] = useState(samplePageInfo);
+
+    const sortedByIndex = (a: BlockInfo, b: BlockInfo) => a.index - b.index
+    const addBlock = ({ type, content, index }: { type: string, content: string, index: number }) => {
+        console.log("addblock")
+        const updateIndex = (prevIndex: number) => prevIndex + Number(prevIndex >= index)
+        setPageInfo(prev => ({
+            ...prev,
+            blocks: [...prev.blocks, { content, type, index, blockId: prev.nextId }].map(block =>
+                ({ ...block, index: updateIndex(block.index) })).sort(sortedByIndex),
+            nextId: prev.nextId + 1,
+        }))
+    }
+    const changeBlock = ({ blockId, type, content }: { blockId: number, type: string, content: string }) => {
+        setPageInfo(prev => ({
+            ...prev,
+            blocks: prev.blocks.map(block =>
+                block.blockId === blockId
+                    ? { ...block, type, content }
+                    : block
+            )
+        }))
+    }
+
     return (
         <PageBox>
-            <BlockContent blockId={1} moveNextBlock={moveNextBlock}>
-                123
-            </BlockContent>
-            <BlockContent blockId={2} moveNextBlock={moveNextBlock}>
-                456
-            </BlockContent>
-            <BlockContent blockId={3} moveNextBlock={moveNextBlock}>
-                789
-            </BlockContent>
+            {pageInfo.blocks.map((block, idx) =>
+                <BlockContent key={block.blockId} blockId={block.blockId} newBlock={addBlock} changeBlock={changeBlock} index={block.index} type={block.type} />)
+            }
         </PageBox>
     )
 }
