f878882b1ae685818549f63bfe1f22748428bb48
ingyobot <woo981216@gmail.com>
2022-11-24 18:50:08 +0900

    [#75]feat: dev서버 새로고침 수정
    dev 서버에서 새로고침이 작동하지 않는 현상 수정
    
    fixes:#75

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -10,7 +10,7 @@ const config = merge(commonConfig, {
     port: 3000,
     historyApiFallback: true,
     proxy: {
-      '/api': {
+      '/': {
         target: 'http://localhost:8080',
         changeOrigin: true,
       },
