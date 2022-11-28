c14c44a630ee2cd139f74b9e911d8d458631dc22
ingyobot <woo981216@gmail.com>
2022-11-28 14:34:51 +0900

    [#88] feat: workspace api router, controller 초기 설정
    workspace 개별 작업 전 공통 설정
    
    resolves:#88

---
+++ server/src/workspace/workspace.router.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+const { authController } = require('../auth/auth.controller');
+const workspaceController = require('./workspace.controller');
+
+const router = Router();
+
+router.use(
+  '/*',
+  authController.verifyAccesstoken,
+  authController.verifyRefreshtoken,
+  authController.requestAccessToken,
+);
+router.get('/list', workspaceController.getList);
+router.post('/addworkspace', workspaceController.addWorkspace);
+router.post('/invite', workspaceController.inviteUser);
+
+module.exports = router;
