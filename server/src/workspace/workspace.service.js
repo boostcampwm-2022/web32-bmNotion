fdc8fc070d2cf28f055933e1465cc56789390b5a
Jeon <jeoninseong8140@gmail.com>
2022-11-29 18:55:20 +0900

    [#104] refactor: workspace response code
     workspace response code 수정

--- server/src/workspace/workspace.service.js
+++ server/src/workspace/workspace.service.js
@@ -5,13 +5,15 @@ const { readOneDocument, updateOneDocument } = require('../db/db.crud');
 const responseMessage = require('../response.message.json');
 const createResponse = require('../utils/response.util');
 
-const readWorkspaceById = async (userId) => {
+const getWorkspacesPipeline = async (userId) => {
   const queryCriteria = {
     $or: [{ owner: userId }, { members: { $elemMatch: { userId: userId } } }],
   };
   const workspaceList = await readAllDocument(dbConfig.COLLECTION_WORKSPACE, queryCriteria);
-  console.log(workspaceList);
-  return workspaceList;
+  const response = createResponse(responseMessage.PROCESS_SUCCESS);
+  response.workspaceList = workspaceList;
+
+  return response;
 };
 
 const inviteUserPipeline = async (userid, workspaceid, nickname) => {
@@ -43,6 +45,6 @@ const inviteUserPipeline = async (userid, workspaceid, nickname) => {
 };
 
 module.exports = {
-  readWorkspaceById,
+  getWorkspacesPipeline,
   inviteUserPipeline
 };
