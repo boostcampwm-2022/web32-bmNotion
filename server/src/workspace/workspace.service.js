f6293608738bf6e6f781f3f32ff1526d2af3c328
ingyobot <woo981216@gmail.com>
2022-11-30 11:01:28 +0900

    [#106] style: 누락된 쉼표 추가

--- server/src/workspace/workspace.service.js
+++ server/src/workspace/workspace.service.js
@@ -61,7 +61,7 @@ const renameWorkspacePipeline = async (userid, workspaceid, workspacename) => {
 };
 
 module.exports = {
-  renameWorkspacePipeline
+  renameWorkspacePipeline,
   getWorkspacesPipeline,
   inviteUserPipeline,
 };
