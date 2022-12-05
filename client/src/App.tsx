567a198e1bb2e41652b8e03ec226543d0a500ddf
hhnn0 <hyun0520ho@gmail.com>
2022-12-06 00:26:40 +0900

    [#162] chore: workSpaceAtom, userAtom 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'jotai';
 import Register from '@/pages/Register';
 import Login from '@/pages/Login';
 import MainPage from '@/pages/MainPage';
@@ -9,15 +10,17 @@ export default function App() {
   return (
     <>
       <GlobalStyle />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />}></Route>
-          <Route path="/signin" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-          <Route path="*" element={<div>404 Not Found Page</div>}></Route>
-          <Route path="/page/:pageid" element={<MainPage />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />}></Route>
+            <Route path="/signin" element={<Login />}></Route>
+            <Route path="/register" element={<Register />}></Route>
+            <Route path="*" element={<div>404 Not Found Page</div>}></Route>
+            <Route path="/page/:pageid" element={<MainPage />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
---
+++ client/src/store/userAtom.tsx
@@ -0,0 +1,16 @@
+import {atom} from 'jotai';
+
+const userInfoAtom = atom({nickName: '', id: ''});
+
+export const userNickNameAtom = atom(
+  (get)=>get(userInfoAtom).nickName,
+  (get, set, newNickName: string)=>{
+    const prev = get(userInfoAtom);
+    set(userInfoAtom, {...prev, nickName: newNickName})
+})
+export const userIdAtom = atom(
+  (get)=>get(userInfoAtom).id,
+  (get, set, newId: string)=>{
+    const prev = get(userInfoAtom);
+    set(userInfoAtom, {...prev, id: newId})
+})
